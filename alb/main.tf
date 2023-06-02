ac2268ebc3cb531914cb11bbbaa5fa95b6ff0655
Dominicfon <dominicnfon@gmail.com>
2023-06-01 21:14:29 -0700

    created target group with elb and HTTP listeners

--- alb/main.tf
+++ alb/main.tf
@@ -29,14 +29,12 @@ resource "aws_lb_target_group" "alb_target_group" {
 }
 
 # create a listener on port 80 with redirect action
-resource "aws_lb_listener" "alb_http_listener" {
-  load_balancer_arn = aws_alb.application_load_balancer.arn
-  port              = 80
-  protocol          = HTTP
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.load_balancer.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-  default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.arn
-
+   default_action {
+    type = "fixed-response"
     }
   }
